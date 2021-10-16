6e76beb7cc4e0c221b2217898020ad539d8dedb2
mikevelko <01146440@pw.edu.pl>
2021-10-16 16:54:37 +0200

    game state abstract class implementation

---
+++ KnightsOfEmpire/KnightsOfEmpire.Common/GameStates/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightsOfEmpire.Definitions.GameStates
+{
+    public abstract class GameState
+    {
+        public virtual void LoadResources() { }
+        public virtual void Initialize() { }
+        public virtual void Update() { }
+        public virtual void Render() { }
+        public virtual void Dispose() { }
+
+    }
+}
