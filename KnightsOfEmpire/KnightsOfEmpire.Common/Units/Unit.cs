159a9ea16a689fb54f55d2f0fdf9f6f9be7f3287
Michał Machnio <michalmachnio.mm@gmail.com>
2021-10-19 20:48:45 +0200

    Added initial classes for map and unit

---
+++ KnightsOfEmpire/KnightsOfEmpire.Common/Units/Unit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightsOfEmpire.Common.Units
+{
+    public class Unit
+    {
+        //TO-DO: uncomment those two fields once SFML.Net will be added to Common Class Library
+        //public Vector2f Position { get; protected set; }
+        //public Vector2f MoveDirection { get; protected set; }
+
+
+        public UnitStats Stats { get; protected set; }
+
+        /// <summary>
+        /// Texture Id which is used to render unit on map 
+        /// </summary>
+        public int TextureId { get; set; }
+
+        /// <summary>
+        /// Unit stance indicates what unit is doing. It can be used to play certain animations.
+        /// </summary>
+        public UnitStance Stance { get; set; }
+
+        
+    }
+}
---
+++ KnightsOfEmpire/KnightsOfEmpire.Common/Units/UnitStance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightsOfEmpire.Common.Units
+{
+    public enum UnitStance
+    {
+        Idle = 0,
+        Moving = 1,
+        Attacking = 2
+    }
+}
