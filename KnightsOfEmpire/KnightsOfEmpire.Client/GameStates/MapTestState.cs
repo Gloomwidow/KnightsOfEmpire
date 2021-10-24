060994bb17d1c39a815ac5ae01fa488588fb43ce
mikevelko <01146440@pw.edu.pl>
2021-10-24 19:13:52 +0200

    mouse moving

--- KnightsOfEmpire/KnightsOfEmpire.Client/GameStates/MapTestState.cs
+++ KnightsOfEmpire/KnightsOfEmpire.Client/GameStates/MapTestState.cs
@@ -20,14 +20,44 @@ namespace KnightsOfEmpire.GameStates
 {
     public class MapTestState : GameState
     {
-        public RectangleShape RectangleShape = new RectangleShape(new Vector2f (1180, 620));
-        public View View = new View(new Vector2f(50, 50), new Vector2f(100, 100));
+        public RectangleShape RectangleShape = new RectangleShape(new Vector2f (800, 800));
+        public View View = new View(new Vector2f(400, 400), new Vector2f(800, 800));
         
         public override void Render()
         {
             Client.RenderWindow.Clear(new Color(100, 50, 247));
             Client.RenderWindow.SetView(View);
-            RectangleShape.Position = new Vector2f(50, 50);
+            //View.Center = ((Vector2f)Mouse.GetPosition(Client.RenderWindow));
+            if(Mouse.GetPosition(Client.RenderWindow).X >= 0 && Mouse.GetPosition(Client.RenderWindow).X <= 50) 
+            {
+                if(View.Center.X >= 300) 
+                {
+                    View.Move(new Vector2f(-0.05f, 0));
+                }      
+            }
+            else if (Mouse.GetPosition(Client.RenderWindow).X >= Client.RenderWindow.Size.X - 50 && Mouse.GetPosition(Client.RenderWindow).X <= Client.RenderWindow.Size.X)
+            {
+                if (View.Center.X <= 700)
+                {
+                    View.Move(new Vector2f(0.05f, 0));
+                }
+            }
+            if (Mouse.GetPosition(Client.RenderWindow).Y >= 0 && Mouse.GetPosition(Client.RenderWindow).Y <= 50)
+            {
+                if (View.Center.Y >= 300)
+                {
+                    View.Move(new Vector2f(0, -0.05f));
+                }
+            }
+            else if (Mouse.GetPosition(Client.RenderWindow).Y >= Client.RenderWindow.Size.Y - 50 && Mouse.GetPosition(Client.RenderWindow).Y <= Client.RenderWindow.Size.Y)
+            {
+                if (View.Center.Y <= 700)
+                {
+                    View.Move(new Vector2f(0, 0.05f));
+                }
+            }
+            //Console.WriteLine(Mouse.GetPosition(Client.RenderWindow).X + Mouse.GetPosition(Client.RenderWindow).Y);
+            RectangleShape.Position = new Vector2f(100, 100);
             RectangleShape.FillColor = new Color(Color.Green);
             Client.RenderWindow.Draw(RectangleShape);
         }
