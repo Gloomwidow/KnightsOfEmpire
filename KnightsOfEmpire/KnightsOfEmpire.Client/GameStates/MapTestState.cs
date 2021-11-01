786d9be914ec0383b3e93d8cd7bed44ee9cd5b74
mikevelko <01146440@pw.edu.pl>
2021-11-01 14:06:42 +0100

    new states for zooming and units selection

--- KnightsOfEmpire/KnightsOfEmpire.Client/GameStates/MapTestState.cs
+++ KnightsOfEmpire/KnightsOfEmpire.Client/GameStates/MapTestState.cs
@@ -24,10 +24,6 @@ namespace KnightsOfEmpire.GameStates
     public class MapTestState : GameState
     {
         public View View = new View(new Vector2f(400, 400), new Vector2f(800, 800));
-        private static float gameZoom = 1;
-        private float minimumZoom = 0.5f;
-        private float maximumZoom = 5.0f;
-        private float gameZoopSpeed = 25.0f;
 
         public Vector2i MousePosition;
         public int EdgeViewMoveOffset = 50;
@@ -44,8 +40,8 @@ namespace KnightsOfEmpire.GameStates
         public List<Texture> textures;
         public RectangleShape[,] mapRectangles;
 
-        public RectangleShape selectionRectangle;
-
+        public UnitsSelectionState UnitsSelectionState;
+        public MouseZoomState MouseZoomState;
 
         public override void Initialize()
         {
@@ -55,8 +51,10 @@ namespace KnightsOfEmpire.GameStates
             textures.Add(new Texture(@"./Assets/Textures/water.png"));
             mapRectangles = new RectangleShape[map.TileCountX, map.TileCountY];
 
-            FlowFieldManager = new FlowFieldManager(map);
+            UnitsSelectionState = new UnitsSelectionState();
+            MouseZoomState = new MouseZoomState();
 
+            FlowFieldManager = new FlowFieldManager(map);
 
             Parallel.For(0, map.TileCountX, (x, stateOuter) =>
             {
@@ -71,7 +69,7 @@ namespace KnightsOfEmpire.GameStates
             ViewCenterRightBoundX = map.TileCountX * Map.TilePixelSize;
             ViewCenterTopBoundY = 0;
             ViewCenterBottomBoundY = map.TileCountY * Map.TilePixelSize;
-            Client.RenderWindow.MouseWheelScrolled += new EventHandler<MouseWheelScrollEventArgs>(OnMouseScroll);
+            MouseZoomState.Initialize();
         }
 
         public void CreateMapRectangle(int x,int y)
@@ -84,68 +82,33 @@ namespace KnightsOfEmpire.GameStates
 
         public override void Update()
         {
-            Vector2i clickPos = Mouse.GetPosition(Client.RenderWindow);
-            Vector2f worldPos = Client.RenderWindow.MapPixelToCoords(clickPos);
-            if (Mouse.IsButtonPressed(Mouse.Button.Left) && selectionRectangle==null)
-            {
-                selectionRectangle = new RectangleShape();
-                selectionRectangle.Position = worldPos;
-                selectionRectangle.FillColor = new Color(0, 0, 255, 50);
-            }
-            else if(Mouse.IsButtonPressed(Mouse.Button.Left) && selectionRectangle != null) 
-            {
-                selectionRectangle.Size = (worldPos - selectionRectangle.Position);
-            }
-            else if(!Mouse.IsButtonPressed(Mouse.Button.Left) && selectionRectangle != null) 
-            {
-                ChangleSelectionRectangleCoords(worldPos);
-                // TO-DO select units in rectangle 
-                selectionRectangle = null;
-            }
+            UnitsSelectionState.Update();
         }
 
-        void OnMouseScroll(object sender, EventArgs e)
-        {
-            RenderWindow window = (RenderWindow)sender;
-            MouseWheelScrollEventArgs mouseEvent = (MouseWheelScrollEventArgs)e;
-            //Console.WriteLine(mouseEvent.Delta);
-            if ((mouseEvent.Delta < 0 && gameZoom >= maximumZoom) || (mouseEvent.Delta > 0 && gameZoom <= minimumZoom)) return;
-            gameZoom -= gameZoopSpeed * mouseEvent.Delta * Client.DeltaTime;
-            gameZoom = Math.Max(minimumZoom, Math.Min(maximumZoom, gameZoom));
-        }
-
-        void ChangleSelectionRectangleCoords(Vector2f endPosition) 
-        {
-            selectionRectangle.Size = new Vector2f(Math.Abs(endPosition.X - selectionRectangle.Position.X), Math.Abs(endPosition.Y - selectionRectangle.Position.Y));
-            selectionRectangle.Position = new Vector2f(Math.Min(endPosition.X, selectionRectangle.Position.X), Math.Min(endPosition.Y, selectionRectangle.Position.Y));
-        }
-
-        void DrawMap() 
-        {
-            for (int i = 0; i < map.TileCountX; i++)
-            {
-                for (int j = 0; j < map.TileCountY; j++)
-                {
-                    Client.RenderWindow.Draw(mapRectangles[i, j]);
-                }
-            }
-        }
 
         public override void Render()
         {
             Client.RenderWindow.Clear(new Color(0,0,0));
             View.Size = new Vector2f(Client.RenderWindow.Size.X, Client.RenderWindow.Size.Y);
-            View.Zoom(gameZoom);
+            View.Zoom(MouseZoomState.gameZoom);
             Client.RenderWindow.SetView(View);
-            MousePosition = Mouse.GetPosition(Client.RenderWindow);
-            float ViewScrollSpeedPerFrame = ViewScrollSpeed * Client.DeltaTime; 
 
-            if (MousePosition.X >= 0 && MousePosition.X <= EdgeViewMoveOffset) 
+            ScrollView();
+            DrawMap();
+            UnitsSelectionState.Render();
+        }
+
+        void ScrollView() 
+        {
+            MousePosition = Mouse.GetPosition(Client.RenderWindow);
+            float ViewScrollSpeedPerFrame = ViewScrollSpeed * Client.DeltaTime;
+
+            if (MousePosition.X >= 0 && MousePosition.X <= EdgeViewMoveOffset)
             {
-                if(View.Center.X >= ViewCenterLeftBoundX)
+                if (View.Center.X >= ViewCenterLeftBoundX)
                 {
                     View.Move(new Vector2f(-ViewScrollSpeedPerFrame, 0));
-                }      
+                }
             }
             else if (MousePosition.X >= (Client.RenderWindow.Size.X - EdgeViewMoveOffset) && MousePosition.X <= Client.RenderWindow.Size.X)
             {
@@ -168,10 +131,16 @@ namespace KnightsOfEmpire.GameStates
                     View.Move(new Vector2f(0, ViewScrollSpeedPerFrame));
                 }
             }
-            DrawMap();
-            if (selectionRectangle != null) 
+        }
+
+        void DrawMap()
+        {
+            for (int i = 0; i < map.TileCountX; i++)
             {
-                Client.RenderWindow.Draw(selectionRectangle);
+                for (int j = 0; j < map.TileCountY; j++)
+                {
+                    Client.RenderWindow.Draw(mapRectangles[i, j]);
+                }
             }
         }
     }
