44d5015e5726e0c7451c763c7c59cfc228a3e14b
Michał Machnio <michalmachnio.mm@gmail.com>
2021-11-03 23:32:21 +0100

    Added packets

---
+++ KnightsOfEmpire/KnightsOfEmpire.Common/Resources/Units/RegisterUnitResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightsOfEmpire.Common.Resources.Units
+{
+    /// <summary>
+    /// A response from server informing client to register new unit on their side
+    /// </summary>
+    [Serializable]
+    public class RegisterUnitResponse
+    {
+        /// <summary>
+        /// To which player belongs that unit
+        /// </summary>
+        public int PlayerId { get; set; }
+        /// <summary>
+        /// ID from player used units
+        /// </summary>
+        public int PlayerUnitId { get; set; }
+        /// <summary>
+        /// Unique id to identify unit during updates
+        /// </summary>
+        public char[] ID { get; set; }
+        public float StartPositionX { get; set; }
+        public float StartPositionY { get; set; }
+    }
+}
